fdb5dd2deb16c67c7715a44bbbf2cf5a82e5ae00
abhpc <37471375+abhpc@users.noreply.github.com>
2019-04-15 23:25:24 +0800

    Update

---
+++ squeue_env.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+# Add these lines in /etc/bashrc or /opt/config/slurm.sh.
+# Output example:
+# JOBID  USER    ST   PARTITION  NAME       SUBMIT_TIME          TIME       NODE CPUS   NODELIST
+# 23     admin   R    Test       sleep      2019-04-15T23:07:11  10:05      2    32     A[36,38]
+#
+
+export SQUEUE_USERS=$USER
+export SQUEUE_FORMAT="%6i %7u %4t %10P %10j %20V %10M %4D %6C %N"
